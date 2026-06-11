e81d179617f0221b6bb7c5dc6e9adc1005a3c2c4
Bodlapati Divya Reddy <divya.innnovations@gmail.com>
2026-06-11 05:35:28 +0530

    Fix sitemap route

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,26 @@
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.divyanso.com/</loc>
+  </url>
+  <url>
+    <loc>https://www.divyanso.com/about</loc>
+  </url>
+  <url>
+    <loc>https://www.divyanso.com/services</loc>
+  </url>
+  <url>
+    <loc>https://www.divyanso.com/projects</loc>
+  </url>
+  <url>
+    <loc>https://www.divyanso.com/contact</loc>
+  </url>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
